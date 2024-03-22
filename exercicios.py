082a541fd269cb64848103c00a402a4bf70d3438
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-22 17:03:57 -0300

    excluido alguns arquivos

--- exercicios.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import urllib.error
-url = "http://esb.gvt.net.br:8888/ResourceManagement/WorkforceManagement/WorkforceManagementReporting/WorkOrderReporting?WSDL"
- 
-try:
-   response = urllib.request.urlopen(url)
-   print(response.read())
-except urllib.error.URLError as e:
-     print(f"Erro na requisção: {e}")
